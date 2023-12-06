22536f2fa9625e4c8fe55c0c8b17cc4ed47811ea
Francisco Juan <juansoussev@gmail.com>
2023-12-06 12:25:07 -0300

    photos page added

--- src/screens/Photos.jsx
+++ src/screens/Photos.jsx
@@ -7,7 +7,27 @@ function Photos() {
   return (
     <>
       <Header />
-      <h1>Photos</h1>
+      <main className="container photos">
+        <ul className="list-photos">
+          <li className="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <img src="/my-princess/photos/1.jpg" alt="" />
+          </li>
+        </ul>
+
+        <div className="modal fade lg" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog modal-fullscreen">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">fotinha</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <img src="/my-princess/photos/1.jpg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
       <ToTop />
     </>
   )

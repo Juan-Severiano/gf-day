311db878fdde4498da07bae9d2f0552444ea8d2a
Francisco Juan <juansoussev@gmail.com>
2023-12-06 15:29:39 -0300

    desktop version done

--- src/screens/Photos.jsx
+++ src/screens/Photos.jsx
@@ -1,28 +1,39 @@
+import { useState } from "react"
 import Header from "../components/Header"
 import ToTop from "../components/ToTop"
+import listPhotos from "../utils/listPhotos"
 
 
 function Photos() {
+  const [photoModal, setPhotoModal] = useState(1)
+  const [name, setName] = useState('')
 
   return (
     <>
       <Header />
       <main className="container photos">
         <ul className="list-photos">
-          <li className="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <img src="/my-princess/photos/1.jpg" alt="" />
-          </li>
+          {listPhotos.map(item => {
+            return (
+              <li className="btn" data-bs-toggle="modal" key={item.id} data-bs-target="#exampleModal" onClick={() => {
+                setPhotoModal(item.id)
+                setName(item.name)
+              }}>
+                <img src={`/my-princess/photos/${item.id}.jpg`} alt="" />
+              </li>
+            )
+          })}
         </ul>
 
         <div className="modal fade lg" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div className="modal-dialog modal-fullscreen">
             <div className="modal-content">
               <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">fotinha</h1>
+                <h1 className="modal-title fs-5 text-black" id="exampleModalLabel">{name}</h1>
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div className="modal-body">
-                <img src="/my-princess/photos/1.jpg" alt="" />
+                <img src={`/my-princess/photos/${photoModal}.jpg`} alt="" />
               </div>
             </div>
           </div>
---
+++ src/utils/listPhotos.js
@@ -0,0 +1,122 @@
+export default [
+    {
+        id: 1,
+        name: 'Bem pimposa na praia',
+    },
+    {
+        id: 2,
+        name: '9x ela 😍',
+    },
+    {
+        id: 3,
+        name: 'hehehe',
+    },
+    {
+        id: 4,
+        name: 'us no onibus',
+    },
+    {
+        id: 5,
+        name: 'ei mano, isso foi fofo',
+    },
+    {
+        id: 6,
+        name: 'bem rebelde',
+    },
+    {
+        id: 7,
+        name: 'ei mano, isso foi fofo',
+    },
+    {
+        id: 8,
+        name: 'meu wallpaper 😘',
+    },
+    {
+        id: 9,
+        name: 'effect',
+    },
+    {
+        id: 10,
+        name: 'us com effect',
+    },
+    {
+        id: 11,
+        name: 'ela no curso bem pimposa',
+    },
+    {
+        id: 12,
+        name: 'eu deformado',
+    },
+    {
+        id: 13,
+        name: 'LINDA DEMAIS',
+    },
+    {
+        id: 14,
+        name: 'dia da viagem',
+    },
+    {
+        id: 15,
+        name: 'bem gatinha no espelho',
+    },
+    {
+        id: 16,
+        name: 'ela no curso again',
+    },
+    {
+        id: 17,
+        name: 'fotinha no espelho',
+    },
+    {
+        id: 18,
+        name: 'no onibus again',
+    },
+    {
+        id: 19,
+        name: 'NOS COM A GATINHA',
+    },
+    {
+        id: 20,
+        name: 'ela linda como sempre',
+    },
+    {
+        id: 21,
+        name: 'GATAAA PERFEITA',
+    },
+    {
+        id: 22,
+        name: 'lindinha no curso',
+    },
+    {
+        id: 23,
+        name: '😚',
+    },
+    {
+        id: 24,
+        name: 'OS OLHINHOS DA GATA',
+    },
+    {
+        id: 25,
+        name: 'FOTINHA NO ESPELHO A GATA',
+    },
+    {
+        id: 26,
+        name: 'us na escola',
+    },
+    {
+        id: 27,
+        name: '',
+    },
+    {
+        id: 28,
+        name: '',
+    },
+    {
+        id: 29,
+        name: '',
+    },
+    {
+        id: 30,
+        name: '',
+    },
+]
